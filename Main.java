91a9fe867a12789b701d16f997a156564bb9ff29
Bianca da Silva Alves <biancadsalves@gmail.com>
2023-11-04 14:23:34 -0300

    Update repo

--- Main.java
+++
@@ -1,8 +0,0 @@
-public class Main {
-    private static EdgeWeightedGraph edgeWeightedGraph;
-
-    public static void main(String[] args) {
-        edgeWeightedGraph = new EdgeWeightedGraph("Tests/basic.txt");
-        System.out.println(edgeWeightedGraph.toDot());
-    }
-}
--- src/Main.java
+++ src/Main.java
@@ -7,7 +7,7 @@ public class Main {
     private static EdgeWeightedGraph edgeWeightedGraph;
 
     public static void main(String[] args) {
-        edgeWeightedGraph = new EdgeWeightedGraph("./Tests/casoteste.txt");
+        edgeWeightedGraph = new EdgeWeightedGraph("./tests/casoteste.txt");
         System.out.println(edgeWeightedGraph.toDot());
 
         DepthFirstSearch dfs = new DepthFirstSearch(edgeWeightedGraph, "hidrogenio");
